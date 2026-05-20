a3b2ccffc4dd4a52054183c90a2352ba4b7fdc24
levanhai05 <152960125+levanhai05@users.noreply.github.com>
2026-05-20 22:15:38 +0700

    Change import from Hibernate to Jakarta Persistence

--- src/main/java/com/example/demo/roles/model/entity/Role.java
+++ src/main/java/com/example/demo/roles/model/entity/Role.java
@@ -3,7 +3,7 @@ package com.example.demo.roles.model.entity;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import org.hibernate.annotations.UuidGenerator;
+import jakarta.persistence.*;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
